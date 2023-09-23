d499809d9385a67d99bbe39bf99cefb648396815
Appiah Blessed <kwabenadicey@gmail.com>
2023-09-23 06:55:32 +0000

    Add shell as provider

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Executes the kill command
 
-exec { 'killmenow':
+exec { 'pkill':
   command  => 'pkill killmenow', # The specific command to execute
   prvoider => 'shell',
   onlyif   => 'pgrep killmenow', # Condition for only if the prvocess is running
