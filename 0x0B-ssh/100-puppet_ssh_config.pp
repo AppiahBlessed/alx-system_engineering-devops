c163418b11b3a11f5f84727a383fdd1bb7b76068
AppiahBlessed <kwabenadicey@gmail.com>
2023-09-27 16:51:25 +0000

    Puppet manifest for ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,16 +5,16 @@ include stdlib
 # Path determines the file path to be changed
 # Line is the specific content to past
 # replace allows for the already data in the file to be oerriden
-file_line { 'Turn off passwd auth'
+file_line { 'Turn off passwd auth':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '      PasswordAuthentication no',
+  line    => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'Declare identity file'
+file_line { 'Declare identity file':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '      IdentifyFile ~/.ssh/school',
+  line    => '    IdentifyFile ~/.ssh/school',
   replace => true,
 }
